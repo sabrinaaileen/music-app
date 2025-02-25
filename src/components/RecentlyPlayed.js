0ed4c1c42d7dee3db413e28520b111b90cb617c6
sabrinaaileen <115700137+sabrinaaileen@users.noreply.github.com>
2025-02-25 22:54:29 +0100

    Add RecentlyPlayed.js and TopHits.js
    Filled the second grid so the user can now see the Top 5 Hits and 5 recently played tracks with the api.

--- src/components/RecentlyPlayed.js
+++ src/components/RecentlyPlayed.js
@@ -1,10 +1,41 @@
 import "../components.css";
+import React, { useState, useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotifyApi = new SpotifyWebApi();
 
 export default function RecentlyPlayed() {
+  const [recentTracks, setRecentTracks] = useState([]);
+
+  useEffect(() => {
+    getRecentTracks();
+  }, []);
+
+  const getRecentTracks = () => {
+    spotifyApi
+      .getMyRecentlyPlayedTracks({ limit: 5 })
+      .then((response) => {
+        if (response && response.items) {
+          console.log(response);
+          setRecentTracks(response.items);
+        }
+      })
+      .catch((error) => {
+        console.log("Error showing recent played tracks: ", error);
+      });
+  };
+
   return (
     <div className="container">
-      <h2>Look at your recent history</h2>
-      <p>Here will be a list of your recent listenings.</p>
+      <h2>Recently played:</h2>
+      <ul>
+        {recentTracks.map((item) => (
+          <li key={item.track.id}>
+            {item.track.name} by{" "}
+            {item.track.artists.map((artist) => artist.name).join(", ")}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/components/TopHits.js
+++ src/components/TopHits.js
@@ -1,10 +1,41 @@
 import "../components.css";
+import React, { useState, useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotifyApi = new SpotifyWebApi();
 
 export default function TopHits() {
+  const [topHits, setTopHits] = useState([]);
+
+  useEffect(() => {
+    getTopHits();
+  }, []);
+
+  const getTopHits = () => {
+    spotifyApi
+      .getMyTopTracks({ limit: 5 })
+      .then((response) => {
+        if (response && response.items) {
+          console.log(response);
+          setTopHits(response.items);
+        }
+      })
+      .catch((error) => {
+        console.log("Error showing top tracks: ", error);
+      });
+  };
+
   return (
     <div className="container">
       <h2>Look at your top hits and artists:</h2>
-      <p>Here will be a list, Top 5 of each</p>
+      <ul>
+        {topHits.map((track) => (
+          <li key={track.id}>
+            {track.name} by{" "}
+            {track.artists.map((artist) => artist.name).join(", ")}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
