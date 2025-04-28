83946d3db6e1c9a1fe57c2407adb0b1e35065655
sabrinaaileen <115700137+sabrinaaileen@users.noreply.github.com>
2025-04-28 15:45:14 +0200

    Update Playlist.js
    Add import of useEffect

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,5 +1,5 @@
 import "../components.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SpotifyWebApi from "spotify-web-api-js";
 import RenamePlaylist from "./RenamePlaylist";
 
